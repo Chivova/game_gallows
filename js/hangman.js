ccdfd7d4ae2434745a20deace71e68518d4b5361
Chivova <Chivovaz@ua.fm>
2021-01-11 22:42:23 +0200

    add game gallows

---
+++ js/hangman.js
@@ -0,0 +1,40 @@
+const words = ['мавпа', 'собака', 'ведмідь', 'котик', 'порося', 'зайченя', 'тюлень', 'корова', 'слон', 'півень', 'лисиця', 'кенгуру', 'черепаха'];
+// выбор случайного слова
+const word = words[Math.floor(Math.random() * words.length)];
+// console.log(word);
+// массив для ответов
+const answerArray = [];
+for (let i = 0; i < word.length; i += 1) { 
+    answerArray[i] = '_';
+}
+
+let remainingLetters = word.length;
+// console.log(answerArray);
+
+// Цикл игры
+while (remainingLetters > 0) { 
+// Показываем игроку его прогресс
+    alert(`Твій прогрес     ${answerArray.join(' ')}`);
+
+    // Вариант ответа от игрока
+    let guess = prompt('Вгадай слово або натисни Cancel, щоб зупинити гру');
+    // guess = guess.toLowerCase();
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert('Будь-ласка введіть одну літеру');
+    } else { 
+        guess = guess.toLowerCase();
+        // Обновляяем процесс игры с помощью переменной guess
+        for (let j = 0; j < word.length; j += 1) { 
+            if (word[j] === guess) { 
+                answerArray[j] = guess;
+                remainingLetters -= 1;
+            }
+        }
+    }
+    // Конец цикла игры
+}
+// Показываем ответ и приветствуем игрока
+alert(answerArray.join(' '));
+alert(`Гарна робота! Відповідь було слово  ${word}`);
